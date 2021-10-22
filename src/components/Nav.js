8bbe664d58f22a64c84b85d8bb9de275e7adacc9
Sabin <baniya.sabinn@gmail.com>
2021-10-22 18:09:59 +0545

    nav bar added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import logo from './logo-dark.svg'
 
 const Nav = () => {
     return (
         <nav>
+            <div>
+                <img src={logo} alt="SB" className="logo" />
+            </div>
+
+            <div className="menuItems">
+                <ul>
+                    <li>Todo Manager</li>
+                    <li>Habit Tracker</li>
+                </ul>
+            </div>
 
 
         </nav>
